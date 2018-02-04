07be57eb733daf806de00bd5ba1ce7e5470a5bf5
sasmita016 <sasmita016@gmail.com>
2018-02-04 10:29:47 +0530

    Adding minor changes

--- HelloWorldInGit.java
+++ HelloWorldInGit.java
@@ -4,7 +4,7 @@ public class HelloWorldInGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hello, World in Git ...");
+		System.out.println("Hello, World in Git ...!!!");
 	}
 
 }
